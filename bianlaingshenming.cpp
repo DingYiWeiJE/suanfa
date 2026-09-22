ddc3658b6a0c0fd5082e9dec16a4c66cdeb6fbc0
dingzhipeng <dingzhipeng@hglaser.com>
2026-09-22 21:34:39 +0800

    开始我们的c++之路

---
+++ bianlaingshenming.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+  int a = 10;
+  double b = 3.14;
+  string s = "hello";
+  bool ok = true;
+  const int n = 100;
+  a = 35;
+  cout << a << "Hello, C++!" << endl;
+  return 0;
+}
